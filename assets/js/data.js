4643bddd45db3f63db25b6a18051788d14ba4999
Helvette <malastrugo@live.cl>
2018-03-13 00:12:38 -0400

    subiendo contenido a través de js, diseño anulado

---
+++ assets/js/data.js
@@ -0,0 +1,119 @@
+const medals = ['', '', '']
+
+const projects = [
+  {//uno
+    title: 'Trivia',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  },
+
+  { //dos
+    title: 'Pokedex',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  },
+
+  { // tres
+    title: 'Prompt Me',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  },
+
+  { // cuatro
+    title: 'Lyft Mobile',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  },
+
+  { // cinco
+    title: 'picBeautifier3000',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  },
+
+  { // seis
+    title: 'Trello',
+    img: '',
+    link: '',
+    decription: '',
+    webapp: true
+  }
+]
+
+
+const skills = [
+ {
+  name: 'HTML5',
+  img: '<i class="fab fa-html5"></i>',
+  learned: true
+ },
+ {
+  name: 'CSS3',
+  img: '<i class="fab fa-css3-alt"></i>',
+  learned: true
+ },
+ {
+  name: 'JavaScript',
+  img: '<i class="fab fa-js-square"></i>',
+  learned: true
+ },
+ {
+  name: 'jQuery',
+  img: '',
+  learned: true
+ },
+ {
+  name: 'Git',
+  img: '<i class="fab fa-git-square"></i>',
+  learned: true
+ },
+ {
+  name: 'Github',
+  img: '<i class="fab fa-github-alt"></i>',
+  learned: true
+ },
+ {
+  name: 'Sass',
+  img: '<i class="fab fa-sass"></i>',
+  learned: true
+ },
+ {
+  name: 'Bootstrap',
+  img: '',
+  learned: true
+ }
+]
+
+
+const contact = [
+  {
+    id: 'mail',
+    link: 'malastrugo@live.cl',
+    img: '<i class="fas fa-envelope"></i>'
+  },
+  {
+    id: 'github',
+    link: 'https://github.com/Helvette',
+    img: '<i class="fab fa-github-alt"></i>'
+  },
+  {
+    id: 'linkedin',
+    link: 'https://www.linkedin.com/in/letferreira/',
+    img: '<i class="fab fa-linkedin-in"></i>'
+  },
+  {
+    id: 'cv',
+    link: '',
+    img: '<i class="fas fa-file-alt"></i>'
+  }
+]
