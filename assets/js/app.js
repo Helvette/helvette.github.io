82711dfa1e23c84bba51840f5670879e8897d679
Helvette <malastrugo@live.cl>
2018-03-26 23:03:17 -0400

    nav estático, con diseño, pero falta funcionalidad de rutas

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,12 +16,21 @@ $('#home').on('mouseenter', '.btn-nav', function() {
   $(this).addClass('for-hover animated infinite flash');
 })
 
+$('#home').on('click', '.btn-nav', function() {
+  $('nav').show()
+})
+
+$('.mi-menu').on('click', '.home', function(){
+  $('nav').hide();
+})
+
 function animating(element, effect){
   $(element).addClass(`animated ${effect}`)
 }
 
 // ejecutar al cargar la página
 $(document).ready(function() {
+  $('nav').hide();
     $('#noise').delay(500).fadeOut();
     $('.content').hide().delay(1000).fadeIn()
     animating('#home .name', 'flipInX')
@@ -29,11 +38,19 @@ $(document).ready(function() {
     animating('#home hr', 'fadeIn')
     animating('#home .sup', 'flipInX')
     $('#pagepiling').pagepiling({
-      menu: '#mi-menu',
+      menu: '.mi-menu',
       direction: 'horizontal',
       anchors: ['home','about', 'skills', 'quests', 'contact'],
       navigation: false,
       normalScrollElements: '#home, #about, #skills, #quests, #contact',
       verticalCentered: false
     });
+    let location = window.location.href;
+    if(location.indexOf('about') != -1 || location.indexOf('skills') != -1 || location.indexOf('quests') != -1 || location.indexOf('contact') != -1){
+      $('nav').show()
+    }
 })
+
+
+
+//while($('#home').css('transform') != 'matrix(1, 0, 0, 1, 0, 0)')
